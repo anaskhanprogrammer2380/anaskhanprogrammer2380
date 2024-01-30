1984733d7d77923c8887a5a0bf1e77446c483d69
anaskhanprogrammer2380 <157421228+anaskhanprogrammer2380@users.noreply.github.com>
2024-01-30 02:00:22 -0800

    Add files via upload

---
+++ bano qabil calculator!.py
@@ -0,0 +1,27 @@
+print("BANO_QABIL CALCULATOR")
+print("")
+print("")
+print("_Calculator for \"ADDITION\"_")
+x=int(input("Enter First Number:"))
+y=int(input("Enter Second Number:"))
+z= x+y
+print(f"Your answer is:\"{z}\"")
+print("")
+print("_Calculator for \"SUBTRACTION\"_")
+x=int(input("Enter First Number:"))
+y=int(input("Enter Second Number:"))
+z= x-y
+print(f"Your answer is:\"{z}\"")
+print("")
+print("_Calculator for \"MULTIPLICATION\"_")
+x=int(input("Enter First Number:"))
+y=int(input("Enter Second Number:"))
+z= x*y
+print(f"Your answer is:\"{z}\"")
+print("")
+print("_Calculator for \"DIVISION\"_")
+x=int(input("Enter First Number:"))
+y=int(input("Enter Second Number:"))
+z= x/y
+print(f"Your answer is:\"{z}\"")
+print("")
